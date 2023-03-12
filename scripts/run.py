46bd89dbd4705d818efff9ac7c182cea0eada047
bolat <muhammetbolat@gmail.com>
2023-03-12 13:18:04 +0300

    run script path are refactored

--- scripts/run.py
+++ scripts/run.py
@@ -1,9 +1,9 @@
 import os
 
 # Prepare dataset
-os.system("python ./prepare_dataset.py --images_dir ../data/TB291/original --output_dir ../data/TB291/VDSR/train --image_size 42 --step 42 --scale 2 --num_workers 10")
-os.system("python ./prepare_dataset.py --images_dir ../data/TB291/original --output_dir ../data/TB291/VDSR/train --image_size 42 --step 42 --scale 3 --num_workers 10")
-os.system("python ./prepare_dataset.py --images_dir ../data/TB291/original --output_dir ../data/TB291/VDSR/train --image_size 44 --step 44 --scale 4 --num_workers 10")
+os.system("python prepare_dataset.py --images_dir ..\data\T91\original --output_dir ..\data\T91\VDSR\\train --image_size 42 --step 42 --scale 2 --num_workers 10")
+#os.system("python prepare_dataset.py --images_dir ../data/TB291/original --output_dir ../data/TB291/VDSR/train --image_size 42 --step 42 --scale 3 --num_workers 10")
+#os.system("python prepare_dataset.py --images_dir ../data/TB291/original --output_dir ../data/TB291/VDSR/train --image_size 44 --step 44 --scale 4 --num_workers 10")
 
 # Split train and valid
-os.system("python ./split_train_valid_dataset.py --train_images_dir ../data/TB291/VDSR/train --valid_images_dir ../data/TB291/VDSR/valid --valid_samples_ratio 0.1")
+os.system("python split_train_valid_dataset.py --train_images_dir ..\data\T91\VDSR\\train --valid_images_dir ..\data\T91\VDSR\\valid --valid_samples_ratio 0.1")
